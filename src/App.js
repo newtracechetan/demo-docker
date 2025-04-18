872e673074eb1be091c9341ff937257b0e1f075b
newtracechetan <chetan@newtrace.io>
2025-04-18 15:32:41 +0530

    🔁 Retry CI/CD one more time

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div >
     <h1>Hello Docker</h1>
-    <h1> NOw you implement a CI/CD Pipeline</h1>
+    <h1> NOw you implement a CI/CD Pipeline retry</h1>
      
     </div>
   );  
